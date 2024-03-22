76d794d9bfe53809d813a1bb4ed73fb3e0a304a8
Akshpreet Singh <akshpreet.singh.02@gmail.com>
2024-03-21 18:36:18 -0600

    updated the date format on event card

--- frontend/src/components/event-card/EventCard.js
+++ frontend/src/components/event-card/EventCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import styles from './event-card.module.css';
 
+function formatDate(dateString) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+}
+
 function EventCard({ event }) {
-  console.log("in event card")
+  console.log("in event card");
   return (
     <div className={styles.eventCard}>
       <div className={styles.eventInfo}>
-        <h2>{event.title}</h2>
-        <p>Start Date: {event.start_date}</p>
-        <p>End Date: {event.end_date}</p>
+        <h2>{event.event_name}</h2>
+        <p>Start Date: {formatDate(event.start_date)}</p>
+        <p>End Date: {formatDate(event.end_date)}</p>
         <p>Location: {event.venue}</p>
-        <p></p>
       </div>
     </div>
   );
